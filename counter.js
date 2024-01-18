62077f4c28d209109a23982227d9eb0ef94286cd
Christina Jasik <cjjasik@gmail.com>
2024-01-18 12:58:18 -0500

    complete vanilla counter with add 1,2,3

--- counter.js
+++ counter.js
@@ -1,7 +1,29 @@
-// Create Button Functionality
+// button var established.
+const buttonOne = document.getElementById("add-one-button");
+const buttonTwo = document.getElementById("add-two-button");
+const buttonThree = document.getElementById("add-three-button");
 
-// ONE BUTTON
+//functionality
+// BUTTON ONE
+function buttonOneClick() {
+    const counterValue = document.getElementById("counter-number");
 
-// TWO BUTTON
+    counterValue.innerHTML = parseInt(counterValue.innerHTML) + 1
+}
+buttonOne.onclick = buttonOneClick;
 
-// THREE BUTTON
+// BUTTON TWO
+function buttonTwoClick() {
+    const counterValue = document.getElementById("counter-number");
+
+    counterValue.innerHTML = parseInt(counterValue.innerHTML) + 2
+}
+buttonTwo.onclick = buttonTwoClick;
+
+// BUTTON THREE
+function buttonThreeClick() {
+    const counterValue = document.getElementById("counter-number");
+
+    counterValue.innerHTML = parseInt(counterValue.innerHTML) + 3
+}
+buttonThree.onclick = buttonThreeClick;
